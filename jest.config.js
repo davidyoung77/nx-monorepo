8af6b54cd87f2f819e9e069c908e46608def14ae
David Young <davidyoung77@gmail.com>
2021-06-01 11:09:11 -0600

    chore: add authentication demo

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  projects: ['<rootDir>/apps/address-book', '<rootDir>/libs/address-book-lib'],
+  projects: [
+    '<rootDir>/apps/address-book',
+    '<rootDir>/libs/address-book-lib',
+    '<rootDir>/apps/google-identity',
+    '<rootDir>/libs/authentication',
+  ],
 };
