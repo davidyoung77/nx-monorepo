eaa9a1addec490087a66bb323023ad5223e5585c
David Young <davidyoung77@gmail.com>
2021-06-01 11:26:41 -0600

    chore: add firebase authentication component

--- libs/authentication/src/lib/authentication.module.ts
+++ libs/authentication/src/lib/authentication.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthenticationComponent } from './authentication.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    AuthenticationComponent
+  ],
+  exports: [
+    AuthenticationComponent
+  ],
 })
 export class AuthenticationModule {}
